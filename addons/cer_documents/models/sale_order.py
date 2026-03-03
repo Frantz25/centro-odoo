c5519a920011c1add058b1c835ced8b49f8f8960
root <root@clawbot.lan>
2026-03-03 15:55:42 +0000

    cer_documents: add quick action to create acceptance acta from sale order

--- addons/cer_documents/models/sale_order.py
+++ addons/cer_documents/models/sale_order.py
@@ -23,18 +23,29 @@ class SaleOrder(models.Model):
             "context": {"default_res_model": "sale.order", "default_res_id": self.id},
         }
 
-    def action_open_cer_document_create_wizard(self):
+    def _cer_document_wizard_action(self, context_updates=None, title=None):
         self.ensure_one()
         ctx = dict(self.env.context or {})
         ctx.update({
             "default_res_model": "sale.order",
             "default_res_id": self.id,
         })
+        if context_updates:
+            ctx.update(context_updates)
         return {
             "type": "ir.actions.act_window",
-            "name": _("Creador Documento CER"),
+            "name": title or _("Creador Documento CER"),
             "res_model": "cer.document.create.wizard",
             "view_mode": "form",
             "target": "new",
             "context": ctx,
         }
+
+    def action_open_cer_document_create_wizard(self):
+        return self._cer_document_wizard_action()
+
+    def action_open_cer_acta_create_wizard(self):
+        self.ensure_one()
+        template = self.env.ref("cer_documents.cer_document_template_acta_aceptacion", raise_if_not_found=False)
+        updates = {"default_template_id": template.id} if template else {}
+        return self._cer_document_wizard_action(updates, _("Crear Acta de Aceptación"))
